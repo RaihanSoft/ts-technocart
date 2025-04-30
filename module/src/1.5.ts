ee168d3f75e773fc03cae3bf303ac6bd889994f0
Raihan <aboraihan14@gmail.com>
2025-04-30 21:20:02 +0600

    objets

---
+++ module/src/1.5.ts
@@ -0,0 +1,13 @@
+//Object
+const user :{
+    companyName : "Blinto LLC"; //literal type
+    firstname : string;
+    middleNane?:string //optional property
+    LastNmae :string;
+    isMarrid : boolean;
+} = {
+    firstname : "Abo",
+    LastNmae : "Raihan",
+    isMarrid : false,
+    companyName : "Blinto LLC",
+}
